fe6b264dd97a39eaea5743384013c6c61884765f
Asad Wali <83274478+Asad-Wali@users.noreply.github.com>
2021-08-04 15:06:34 +0500

    styling orgpage

---
+++ src/app/org-emp/org-emp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-org-emp',
+  templateUrl: './org-emp.component.html',
+  styleUrls: ['./org-emp.component.css']
+})
+export class OrgEmpComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
